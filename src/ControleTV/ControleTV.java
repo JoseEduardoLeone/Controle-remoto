34c79264de0c0c9869f0e367cb91de493a213ffc
José Eduardo <eduard.leone21@gmail.com>
2022-01-26 02:35:59 -0300

    Controle remoto Simples

---
+++ src/ControleTV/ControleTV.java
@@ -0,0 +1,17 @@
+package ControleTV;
+
+public class ControleTV {
+    public static void main (String[] args){
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.desligarMudo();
+        c.desligar();
+
+
+
+
+
+
+
+    }
+}
